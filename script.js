7ffa3d4454c7db70b4ef966e9651e9152db34e71
alanisvalentim <alanisvalentim@hotmail.com>
2018-10-30 22:00:11 -0300

    primeiro js

---
+++ script.js
@@ -0,0 +1,29 @@
+// let nomeDoUsuario = "Alanis Valentim";
+// let idade = 23;
+// let vivo = false;
+
+// let saida1 = "Olá," + nome + "! Tudo bem com você?";
+// let saida1 = `Olá, ${nome}! Você tem ${idade} anos!`;
+
+// console.log(prompt('Qual seu nome?'));
+
+
+// idade = prompt ("Quantos anos você tem?");
+// console.log (`Você tem ${idade}`);
+
+
+// vivo = confirm("Você está vivo?")
+
+let cachorro = 20;
+let gato = 10;
+
+
+cachorro = number (prompt("Quantos cachorros você tem?"));
+ alert (`Você tem ${cachorro} cachorros!`);
+
+gato = number (prompt("Quantos gatos você tem?"));
+alert (`Você tem ${gato} gatos!`);
+
+alert(gato+cachorro);
+
+
